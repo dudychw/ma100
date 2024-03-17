88585c34974f2e3b989b39a4c30b5052010f6177
dedgkhusic <ataev-05@bk.ru>
2024-03-17 12:40:50 +0300

    fix main

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import datetime
 from internal.trading import Trading
 from pkg.logger import Logger
 from test.history_test import HistoryTest
-# try:
-#     Trading().start()
-# except Exception as err:
-#     Logger().logger('ERROR', err)
-start = datetime.datetime.now()
-HistoryTest().history_test()
-print(datetime.datetime.now() - start)
+try:
+    Trading().start()
+except Exception as err:
+    Logger().logger('ERROR', err)
+# start = datetime.datetime.now()
+# HistoryTest().history_test()
+# print(datetime.datetime.now() - start)
