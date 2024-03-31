7c8f156df1fae67a003815f77ea55fa1e9577aab
dedgkhusic <ataev-05@bk.ru>
2024-03-31 05:20:10 +0300

    added history_logs.txt

--- pkg/logger.py
+++ pkg/logger.py
@@ -4,7 +4,9 @@ import datetime
 class Logger:
 
     def logger(self, status, message):
-        with open(f'../ma100/doc/logger.txt', 'a', encoding="utf-8") as file:
+        with open(f'../ma100/logs/logger.txt', 'a', encoding="utf-8") as file:
             file.write(f'{datetime.datetime.now()} [{status}]: text: {message} \n')
             file.close()
-
+        with open(f'../ma100/logs/history_logs.txt', 'a', encoding="utf-8") as file:
+            file.write(f'{datetime.datetime.now()} [{status}]: text: {message} \n')
+            file.close()
