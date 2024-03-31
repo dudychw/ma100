1ba6abef241d2ffc918890000fb6e60a237baf5a
dedgkhusic <ataev-05@bk.ru>
2024-03-31 09:24:43 +0300

    comment all history logs

--- pkg/logger.py
+++ pkg/logger.py
@@ -7,6 +7,6 @@ class Logger:
         with open(f'../ma100/logs/logger.txt', 'a', encoding="utf-8") as file:
             file.write(f'{datetime.datetime.now()} [{status}]: text: {message} \n')
             file.close()
-        with open(f'../ma100/logs/history_logs.txt', 'a', encoding="utf-8") as file:
-            file.write(f'{datetime.datetime.now()} [{status}]: text: {message} \n')
-            file.close()
+        # with open(f'../ma100/logs/history_logs.txt', 'a', encoding="utf-8") as file:
+        #     file.write(f'{datetime.datetime.now()} [{status}]: text: {message} \n')
+        #     file.close()
